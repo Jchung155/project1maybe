eb16d0166d4efb4b36cf6543aae65c554c52707c
Jchung155 <jdpchung@gmail.com>
2025-03-11 14:11:06 -0400

    final push i guess

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
 
-public class StationaryEnemyScript : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
+
+public float speed;
+public GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+       transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -19,6 +23,7 @@ public class StationaryEnemyScript : MonoBehaviour
         if (other.gameObject.CompareTag("Bullet"))
         {
             transform.gameObject.tag = "Untagged";
+            Destroy(this.gameObject);
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,25 +9,41 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI moneyText;
     public GameObject player;
     public PlayerController playerScript;
+    public GameObject enemy;
+    public float spawnTime;
+    public float lastSpawnTime;
 
     void Start()
     {
-        
+        spawnEnemy();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { if(playerScript.dead) return;
+       updateDepth();
+
+       lastSpawnTime-=Time.deltaTime;
+
+       if (lastSpawnTime <=0){
+              spawnEnemy();
+              lastSpawnTime = spawnTime;
+              Debug.Log("hi");
+       }
     }
 
     private void updateDepth()
     {
-
+   depthText.text = "Meters: " + (int)player.transform.position.y;
     }
 
     private void updateScore()
     {
 
+    }
+
+    private void spawnEnemy(){
+        Vector2 playerPosition = new Vector3(player.transform.position.x, player.transform.position.y-10, 0);
+        Instantiate(enemy, playerPosition, Quaternion.identity);
     }
 }
